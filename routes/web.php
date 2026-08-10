2f353a3302e23acb754a9fe946b217dcb8cd8f24
dharmayudha <dharmayudha293@gmail.com>
2026-08-10 15:57:17 +0700

    fix: Use redirect()->route('login') for root URL to automatically preserve subfolder path

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,9 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\DetailUserController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
-Route::redirect('/', '/login');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     // Dashboard
